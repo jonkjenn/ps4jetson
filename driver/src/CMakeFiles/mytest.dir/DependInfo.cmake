426d3354d013623e273086d92d5943c4e1dd7690
Jon Kjennbakken <jonkjenn@gmail.com>
2015-04-07 12:50:09 +0000

    Generalized cmake

--- driver/src/CMakeFiles/mytest.dir/DependInfo.cmake
+++ driver/src/CMakeFiles/mytest.dir/DependInfo.cmake
@@ -1,26 +1,26 @@
 # The set of languages for which implicit dependencies are needed:
-set(CMAKE_DEPENDS_LANGUAGES
+SET(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/jon/code/PS4EYECam/driver/src/test.cpp" "/home/jon/code/PS4EYECam/driver/src/CMakeFiles/mytest.dir/test.cpp.o"
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/ps4jetson/driver/src/test.cpp" "/home/ubuntu/ps4jetson/driver/src/CMakeFiles/mytest.dir/test.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/jon/code/PS4EYECam/driver/src/CMakeFiles/ps4eye.dir/DependInfo.cmake"
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ubuntu/ps4jetson/driver/src/CMakeFiles/ps4eye.dir/DependInfo.cmake"
   )
 
 # The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
+SET(CMAKE_C_TARGET_INCLUDE_PATH
   "/usr/include/opencv"
   "ps4eye"
   "../include"
   "."
-  "/home/jon/code/PS4EYECam/libusb/libusb"
+  "../../libusb/libusb"
   )
-set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- driver/src/CMakeFiles/ps4eye.dir/DependInfo.cmake
+++ driver/src/CMakeFiles/ps4eye.dir/DependInfo.cmake
@@ -1,25 +1,25 @@
 # The set of languages for which implicit dependencies are needed:
-set(CMAKE_DEPENDS_LANGUAGES
+SET(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/jon/code/PS4EYECam/driver/src/ps4eye.cpp" "/home/jon/code/PS4EYECam/driver/src/CMakeFiles/ps4eye.dir/ps4eye.cpp.o"
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/ps4jetson/driver/src/ps4eye.cpp" "/home/ubuntu/ps4jetson/driver/src/CMakeFiles/ps4eye.dir/ps4eye.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+SET(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
+SET(CMAKE_C_TARGET_INCLUDE_PATH
   "/usr/include/opencv"
   "ps4eye"
   "../include"
   "."
-  "/home/jon/code/PS4EYECam/libusb/libusb"
+  "../../libusb/libusb"
   )
-set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
